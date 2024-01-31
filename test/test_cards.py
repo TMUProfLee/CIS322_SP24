3d16134bbdc9b65de31e2614fb219b43279c4fc4
Justyn Lee <justynlee@sbcglobal.net>
2024-01-30 20:49:24 -0800

    updates test files

--- test/test_cards.py
+++ test/test_cards.py
@@ -1,5 +1,5 @@
 from testing_base import *
-
+# Justyn Lee
 def test_show_hand():
 
     matthew = Player("Matthew")
@@ -8,6 +8,20 @@ def test_show_hand():
     four_spades = getCard("Spades", 4)
     three_spades = getCard("spades", 3)
     two_spades = getCard("Spades", 2)
+    one_spades = getCard("Spades", 1)
+    cards = [ five_spades, four_spades, three_spades, two_spades, one_spades ]
+    
+    matthew.setHand( cards, isKnown=True )
+    assert matthew.showHand() == None
+
+def test_show_hand_face_cards():
+
+    matthew = Player("Matthew")
+
+    five_spades = getCard("spades", 10)
+    four_spades = getCard("Spades", 11)
+    three_spades = getCard("spades", 12)
+    two_spades = getCard("Spades", 13)
     one_spades = getCard("Spades", 1)
     cards = [ five_spades, four_spades, three_spades, two_spades, one_spades ]
     

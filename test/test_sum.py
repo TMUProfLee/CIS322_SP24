a3a93d95ac4da4c5e293593ddd775b7a9e121176
Erin Yoo <yooeh@masters.edu>
2024-01-30 21:54:37 -0800

    adds second test function

--- test/test_sum.py
+++ test/test_sum.py
@@ -14,25 +14,18 @@ def test_summation():
     
     expect_value = 10
     assert total == expect_value, "Test failed: Summation incorrect"
-    
-test_summation()
 
-# def empty_summation():
-#     matthew = Player("Matthew")
-#     # input: list of hand of cards
+def test_summation2():
+    matthew = Player("Matthew")
+    # input: list of hand of cards
     
-#     empty_cards = [getCard('Clubs', 0)]
-#     matthew.setHand(empty_cards, isKnown=True)
+    empty_cards = []
+    matthew.setHand(empty_cards, isKnown=True)
     
-#     # call the sum_cards()
-#     total2 = sum_cards(matthew.hand)
-#     print(f"Total sum of cards in Matthew's hand: {total2}")
+    # call the sum_cards()
+    total2 = sum_cards(matthew.hand)
+    print(f"Total sum of cards in Matthew's hand: {total2}")
     
-#     expect_val = 0
-#     assert total2 == expect_val, "Test failed: Summation incorrect"
+    expect_val = 0
+    assert total2 == expect_val, "Test failed: Summation incorrect"
 
-
-if __name__ == "__main__":
-    test_summation()
-    #empty_summation()
-    print("All tests passed")

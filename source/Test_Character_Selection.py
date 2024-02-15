4cc1d530535d09feef2551c6ca148938d4dd1438
Ethan Thomas <e.thomas0523@gmail.com>
2024-02-15 13:24:24 -0800

    Character Selection

---
+++ source/Test_Character_Selection.py
@@ -0,0 +1,10 @@
+from CardGames import character_selection
+
+def test_character_selection():
+    #This function requires user input
+    character_selection
+
+
+#Ran this file two times to show that there can be different results based on user input
+test_character_selection()
+    

3ba575cfbfe255ef750097a4dbf6eeebfc07f9ed
Ian Estrella <107520125+youraveragedeveloper7@users.noreply.github.com>
2024-02-20 18:49:18 -0800

    Sanitized input
    Sanitized the input a little bit.

--- source/Game_Setup.py
+++ source/Game_Setup.py
@@ -1,4 +1,4 @@
-from NewCardGames import *
+from source.NewCardGames import *
 
 class GameSetup:
     def __init__(self):
@@ -9,23 +9,26 @@ class GameSetup:
         self.fugitive_deck.append(escape_card)
         #Evolve this into creating a Marshall object (deck should be empty by default
         self.marshall_deck = []
-
+        self.fugitive_name = ""
+        self.marshall_name = ""
         self.done = False
     
     def start_game(self):
         #Evolve this into accessing Fugitive object's 'Name' attribute and setting it to input
-        fugitive_name = input("Fugitive, what is your name? --> ")
-        marshall_name = input("Marshall, what is your name? --> ")
-        
+        while len(self.fugitive_name) == 0:
+            self.fugitive_name = input("Fugitive, what is your name? --> ").strip()
+        while len(self.marshall_name) == 0:
+            self.marshall_name = input("Marshall, what is your name? --> ").strip()
+        print(f"{self.fugitive_name} will go first!")
         first_turn = True
 
         while self.done != True:
             #Different rule sequence for first turn
             while first_turn:
                 #Fugitive places 1 or 2 new hideouts
-
+                print(f"{self.fugitive_name} please choose a hideout to pick.")
                 #Marshall draws 2 cards; chooses which deck to draw from
-
+                print(f"{self.marshall_name} please choose which deck to draw your cards from.")
             #Outside of first turn
             #Fugitive draws 1 card from any deck. 
             #Can choose to place 1 new hideout, or pass
@@ -33,16 +36,8 @@ class GameSetup:
             #Marshall draws 1 card from any deck.
             #Makes a singular guess to find a hideout (possible expand into guessing multiple hideouts)
         
-                
+                return
             #Fugitive's Win Condition --> Fugitive plays card with value 42.
             #Marshall's Win Condition --> Wins by guessing all cards (hideouts) currently in play
-            
-
-            
+            return
         
-
-        
-
-game = GameSetup()
-game.start_game()
-

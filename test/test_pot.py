9a3678754344d1234bf0c491e209cc9ab13b1bab
Karlie McCue <123039174+mccueki@users.noreply.github.com>
2024-02-20 19:55:08 -0800

    test file

--- test/test_pot.py
+++ test/test_pot.py
@@ -10,7 +10,40 @@ def test_pot():
     Call(billy, 10, pot)
     print(pot.show_pot())
 
-    casey.addMoney(50)
+    casey.addMoney(60)
     Call(casey, 10, pot)
     print(pot.show_pot())
+
     assert pot.show_pot() == 20
+
+
+def test_billy():
+    billy = Player("Billy")
+    casey = Player("Casey")
+    pot = Pot()
+
+    billy.addMoney(50)
+    Call(billy, 10, pot)
+    print(pot.show_pot())
+
+    casey.addMoney(60)
+    Call(casey, 10, pot)
+    print(pot.show_pot())
+
+    assert billy.money == 40
+
+def test_casey():
+    billy = Player("Billy")
+    casey = Player("Casey")
+    pot = Pot()
+
+    billy.addMoney(50)
+    Call(billy, 10, pot)
+    print(pot.show_pot())
+
+    casey.addMoney(60)
+    Call(casey, 10, pot)
+    print(pot.show_pot())
+
+    assert casey.money == 50
+  

c9df08b1732f1b1a19aca898d54ab35f7974f613
Sean Custodero <custoderosd@masters.edu>
2024-02-20 20:37:02 -0800

    Add marshall first turn function

---
+++ source/marshall_setup.py
@@ -0,0 +1,64 @@
+from source.CardGames import *
+
+marshall = Player("Marshall")
+deck = Deck()
+
+def marshall_first_turn():
+    # Waiting for deck to be divided into three groups
+    input("Select which deck to draw two cards from ['1' (1-14), '2' (15-28), '3' (29-42)] and press enter...")
+
+    for i in range(2):
+        draw_card = deck.getCard()
+        marshall.addCard(draw_card, isKnown=True)
+
+    marshall.showHand()
+
+    #Display board
+    
+    while(True):
+        guess_all = input("Would you like to guess all fugitive locations? (y/n)")
+        if guess_all.lower() == 'y' or guess_all.lower() == 'yes':
+            while(True):
+                guess = input("Enter locations separated only by a comma (1,2,3...)").split(',')
+                """
+                if len_list < placed_locations:
+                    print("Must guess all locations")
+                    guess = input("Enter locations separated only by a comma (1,2,3...)").split(',')
+                    continue
+                """
+                # Check that input is a valid list of ints
+                bad = False
+                for ele in guess:
+                    try:
+                        ele = int(ele)
+                    except:
+                        TypeError
+                        print("Invalid input, try again")
+                        bad = True
+                        break
+                if bad:
+                    continue
+                break
+            break
+        elif guess_all.lower() == 'n' or guess_all.lower() == 'no':
+            while True:
+                try:
+                    guess = int(input("Enter futitive location..."))
+                except:
+                    ValueError
+                    print("Invalid input, try again")
+                    continue
+                break
+            break
+        else:
+            print("Invalid input, try again")
+
+    #marshall.add_guess(guess)
+    print("Guesses added")
+    """
+    if check_location(marshall_current_idx, guess):
+        print("Correct location!")
+        reveal_location(marshall_current_idx)
+    else:
+        print("Incorrect guess.")
+    """
---
+++ test/test_marshall_first_turn.py
@@ -0,0 +1,4 @@
+from testing_base import *
+from source.marshall_setup import *
+
+marshall_first_turn()

0d6894c3375b7bd0c2cb1f3c968148a5e0af700e
Ethan Thomas <e.thomas0523@gmail.com>
2024-02-22 18:27:30 -0800

    Update CardGames.py

--- source/CardGames.py
+++ source/CardGames.py
@@ -153,6 +153,165 @@ class Dealer:
     self.deck.reset()
     self.deck.shuffle()
 
+
+import random
+import os
+
+cardImages = []
+values = list(range(1,14))
+suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+def find_root_dir():
+  cwd = os.getcwd()
+  while 'source' not in os.listdir():
+    os.chdir('..')
+    cwd = os.path.join( cwd, '..')
+  return cwd
+
+class Card:
+  def __init__(self, suit, value, image, cardBack):
+    self.cardBack = cardBack
+    self.suit = suit
+    self.value = value
+    self.image = image
+    self.shortImage = []
+    if self.image:
+      for line in self.image:
+        self.shortImage.append(line[:4])
+
+  def __eq__(self, other):
+    if not type(other) == Card:
+      return False
+    return self.suit == other.suit and \
+      self.value == other.value
+
+class Deck:
+  def __init__(self):
+    root_dir = os.path.join( find_root_dir(), 'source')
+    cards_file = f'{root_dir}{os.path.sep}playing_cards.txt'
+    with open(cards_file, "r") as cards:
+      cardBack = []
+      for _ in range(6):
+        line = cards.readline()
+        cardBack.append(line.replace("\n",""))
+      card = []
+      level = 0
+      for line in cards.readlines():
+        if len(line) == 1:
+          cardImages.append(card)
+          level = 0
+          card = []
+          continue
+        card.append(line.replace("\n",""))
+        level += 1
+      cardImages.append(card)
+    
+    deck = []
+    index = 0
+    for suit in suits:
+      for value in values:
+        deck.append(Card(suit, value, cardImages[index], cardBack))
+        index += 1
+    
+    self.cards = deck
+    self.size = len(deck)
+    self.cardBack = cardBack
+    self.discarded = []
+
+  def reset(self):
+    self.cards += self.discarded
+    self.discarded = []
+    self.size = len(self.cards)
+
+  def shuffle(self):
+    random.shuffle(self.cards)
+
+  def getCard(self):
+    card = self.cards.pop()
+    self.size -= 1
+    self.discarded.append(card)
+    return card
+
+def getCard( suit, value):
+  deck = Deck()
+  my_card = Card( suit.capitalize(), value, None, None)
+  for card in deck.cards:
+    if card == my_card:
+      return card
+  return None
+
+class Player:
+  def __init__(self, name, money: int = 0):
+    self.name = name
+    self.hand = []
+    self.knownCards = []
+    self.money = money
+
+  def addMoney(self, amount: int):
+    self.money += amount
+    return self.money
+
+  def makeBet(self, amount: int):
+    if amount > self.money:
+      print("%s does not have enough money to make this bet." % self.name)
+      return self.money
+    self.money -= amount
+    return self.money
+
+  def addCard(self, card: Card, isKnown: bool = True):
+    self.hand.append(card)
+    if isKnown:
+      self.knownCards.append(True)
+    else:
+      self.knownCards.append(False)
+
+  def setHand(self, cards: "list[Card]", isKnown: bool = False):
+    self.hand = cards
+    self.knownCards = [isKnown for _ in self.hand]
+
+  def showHand(self, printShort: bool = False):
+    for idx in range(6):
+      for i, card in enumerate(self.hand):
+        if printShort and i < len(self.hand)-1:
+          image = card.shortImage[idx]  if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+      print()
+
+  def clearHand(self):
+    self.hand = []
+    self.knownCards = []
+
+class Dealer:
+  def __init__(self, deck: Deck):
+    self.deck = deck
+    self.deck.shuffle()
+
+  def printCards(self, cards: "list[Card]", showFront: bool, printShort: bool = True):
+    for idx in range(6):
+      for i, card in enumerate(cards):
+        if printShort and i < len(cards)-1:
+          image = card.shortImage[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+      print()
+
+  def dealCards(self, numCards: int, players: "list[Player]"):
+    if numCards * len(players) > self.deck.size:
+      return False
+    for player in players:
+      for _ in range(numCards):
+        player.addCard(self.deck.getCard())
+    return True
+
+  def resetDeck(self):
+    self.deck.reset()
+    self.deck.shuffle()
+    
 def character_selection():
   Player1 = input("Player 1, please enter your name: ")
   Player2 = input("Player 2, please enter your name: ")
@@ -160,26 +319,27 @@ def character_selection():
   Player2Role = ""
   rand = random.randint(0,1)
   if rand == 0:
-    while Player1Role != "Fugitive" and Player1Role != "fugitive" and Player1Role != "Marshall" and Player1Role != "marshall":
-      Player1Role = input(str(Player1) + ", please pick your role(Fugitive or Marshall): ")
-      if Player1Role == "Fugitive" or Player1Role == "fugitive":
+    while Player1Role != "fugitive" and Player1Role != "marshall":
+      Player1Role = input(str(Player1) + ", please pick your role(Fugitive or Marshall): ").lower()
+      if Player1Role == "fugitive":
         print(str(Player1) + ", you are the Fugitive.")
         print(str(Player2) + ", you are the Marshall.")
-      elif Player1Role == "Marshall" or Player1Role == "marshall":
+      elif Player1Role == "marshall":
         print(str(Player1) + ", you are the Marshall.")
         print(str(Player2) + ", you are the Fugitive.")
   else:
-    while Player2Role != "Fugitive" and Player2Role != "fugitive" and Player2Role != "Marshall" and Player2Role != "marshall":
-      Player2Role = input(str(Player2) + ", please pick your role(Fugitive or Marshall): ")
-      if Player2Role == "Fugitive" or Player2Role == "fugitive":
+    while Player2Role != "fugitive" and Player2Role != "marshall":
+      Player2Role = input(str(Player2) + ", please pick your role(Fugitive or Marshall): ").lower()
+      if Player2Role == "fugitive":
         print(str(Player2) + ", you are the Fugitive.")
         print(str(Player1) + ", you are the Marshall.")
-      elif Player2Role == "Marshall" or Player2Role == "marshall":
+      elif Player2Role == "marshall":
         print(str(Player2) + ", you are the Marshall.")
         print(str(Player1) + ", you are the Fugitive.")
-      else:
-        Player2Role = input("Please pick a proper role(Fugitive or Marshall): ")
-  return Player1, Player2, Player1Role, Player2Role
+
+character_selection()
+    
+
 
 def highestCard(cardList):
   highestCard = getCard("Spades", 1)

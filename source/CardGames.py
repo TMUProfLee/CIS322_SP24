6cc74f751fed80b94d40b46e63baa2b2317c958a
SCustodero <68943688+SCustodero@users.noreply.github.com>
2024-02-26 10:32:53 -0800

    Update CardGames.py

--- source/CardGames.py
+++ source/CardGames.py
@@ -1,9 +1,11 @@
-
 import random
 import os
 
 cardImages = []
+values = list(range(1,14))
+suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 values = list(range(1,43))
+sprint_value = [1, 2]
 
 def find_root_dir():
   cwd = os.getcwd()
@@ -11,28 +13,27 @@ def find_root_dir():
     os.chdir('..')
     cwd = os.path.join( cwd, '..')
   return cwd
-
+#Card class adjusted
 class Card:
-  def __init__(self, value, burn, image, cardBack):
+  def __init__(self, sprint_value, value, image, cardBack):
     self.cardBack = cardBack
+    self.sprint_value = sprint_value
     self.value = value
-    self.burn = burn
     self.image = image
     self.shortImage = []
     if self.image:
       for line in self.image:
         self.shortImage.append(line[:4])
-
   def __eq__(self, other):
     if not type(other) == Card:
       return False
     return self.value == other.value and \
-      self.burn == other.burn
-
+      self.sprint_value == other.sprint_value
+#Deck class adjusted
 class Deck:
   def __init__(self):
     root_dir = os.path.join( find_root_dir(), 'source')
-    cards_file = f'{root_dir}{os.path.sep}playing_cards.txt'
+    cards_file = f'{root_dir}{os.path.sep}new_playing_cards.txt'
     with open(cards_file, "r") as cards:
       cardBack = []
       for _ in range(6):
@@ -53,92 +54,68 @@ class Deck:
     deck = []
     index = 0
     for value in values:
-      burn = 2 if value % 2 == 0 else 1
-      deck.append(Card(value, burn, cardImages[index], cardBack))
-      index += 1
+        if value % 2 == 0:
+            deck.append(Card(2, value, cardImages[index], cardBack))
+        else:
+            deck.append(Card(1, value, cardImages[index], cardBack))
+        index += 1
     
     self.cards = deck
     self.size = len(deck)
     self.cardBack = cardBack
     self.discarded = []
-
   def reset(self):
     self.cards += self.discarded
     self.discarded = []
     self.size = len(self.cards)
-
   def shuffle(self):
     random.shuffle(self.cards)
-
   def getCard(self):
     card = self.cards.pop()
     self.size -= 1
     self.discarded.append(card)
     return card
-
-def getCard(value, burn):
-  deck = Deck()
-  my_card = Card(value, burn, None, None)
-  for card in deck.cards:
-    if card == my_card:
-      return card
-  return None
-
 class Player:
   def __init__(self, name, money: int = 0):
     self.name = name
     self.hand = []
     self.knownCards = []
     self.money = money
-
   def addMoney(self, amount: int):
     self.money += amount
     return self.money
-
   def makeBet(self, amount: int):
     if amount > self.money:
       print("%s does not have enough money to make this bet." % self.name)
       return self.money
     self.money -= amount
     return self.money
-
   def addCard(self, card: Card, isKnown: bool = True):
     self.hand.append(card)
     if isKnown:
       self.knownCards.append(True)
     else:
       self.knownCards.append(False)
-
   def setHand(self, cards: "list[Card]", isKnown: bool = False):
     self.hand = cards
     self.knownCards = [isKnown for _ in self.hand]
-
   def showHand(self, printShort: bool = False):
     for idx in range(6):
       for i, card in enumerate(self.hand):
-        """
         if printShort and i < len(self.hand)-1:
           image = card.shortImage[idx]  if self.knownCards[i] else card.cardBack[idx]
           print(image, end="")
         else:
-        """
-        image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
-        print(image, end="")
-      
+          image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
       print()
-    if self.name == "Fugitive":
-      input("\nPress enter to hide hand...")
-      os.system('cls')
-
   def clearHand(self):
     self.hand = []
     self.knownCards = []
-
 class Dealer:
   def __init__(self, deck: Deck):
     self.deck = deck
     self.deck.shuffle()
-
   def printCards(self, cards: "list[Card]", showFront: bool, printShort: bool = True):
     for idx in range(6):
       for i, card in enumerate(cards):
@@ -149,7 +126,6 @@ class Dealer:
           image = card.image[idx] if showFront else card.cardBack[idx]
           print(image, end="")
       print()
-
   def dealCards(self, numCards: int, players: "list[Player]"):
     if numCards * len(players) > self.deck.size:
       return False
@@ -157,15 +133,6 @@ class Dealer:
       for _ in range(numCards):
         player.addCard(self.deck.getCard())
     return True
-
   def resetDeck(self):
     self.deck.reset()
     self.deck.shuffle()
-
-
-def highestCard(cardList):
-  highestCard = getCard("Spades", 1)
-  for card in cardList:
-    if card.value >= highestCard.value:
-      highestCard = card
-  return highestCard
